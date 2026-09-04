df82e77481d9d791b49288aeea5b8044fe676f68
Ashimolowo <ashimolowoayobami@gmail.com>
2026-09-04 02:38:12 -0700

    Replaced the scrollview with REACT-NATIVE FLATLIST component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Pressable, StyleSheet, ScrollView } from "react-native";
+import { View, Text, Pressable, StyleSheet, ScrollView, FlatList } from "react-native";
 import { UserButton } from "@clerk/expo/native";
 import { Link } from "expo-router";
 import { Show, useUser } from "@clerk/expo";
@@ -9,35 +9,61 @@ import PendingItemCard from "@/components/list/PendingItemCard";
 import CompletedItems from "@/components/list/CompletedItems";
 
 export default function Home() {
-const {items} = useGgoceriesStore()
+  const { items } = useGgoceriesStore();
 
-const pendingItems = items.filter((item) => !item.purchased)
+  const pendingItems = items.filter((item) => !item.purchased);
+  //   return (
+  //     <ScrollView
+  //       className="flex-1 bg-background py-4"
+  //       showsVerticalScrollIndicator={false}
+  //       contentContainerStyle={{ padding: 20, gap: 14 }}
+  //     >
+  //       <TabScreenBackground />
+
+  //       <ListHeroCard />
+
+  //       <View className="flex-row items-center justify-between px-1">
+  //         <Text className="text-sm font-semibold uppercase tracking-[1px] text-muted-foreground">
+  //           Shopping items
+  //         </Text>
+  //         <Text className="text-sm text-muted-foreground">
+  //           {pendingItems.length} active
+  //         </Text>
+  //       </View>
+
+  //       {pendingItems.map((item) => (
+  //         <PendingItemCard key={item.id} item={item} />
+  //       ))}
+
+  //       <CompletedItems />
+  //     </ScrollView>
+  //   );
+  // }
+
   return (
-    <ScrollView
-      className="flex-1 bg-background py-4"
-      showsVerticalScrollIndicator={false}
+    <FlatList
+      className="flex-1 bg-background "
+      data={pendingItems}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PendingItemCard item={item} />}
       contentContainerStyle={{ padding: 20, gap: 14 }}
-    >
-      <TabScreenBackground />
-
-      <ListHeroCard />
-
-      <View className="flex-row items-center justify-between px-1">
-        <Text className="text-sm font-semibold uppercase tracking-[1px] text-muted-foreground">
-          Shopping items
-        </Text>
-        <Text className="text-sm text-muted-foreground">
-          {pendingItems.length} active
-        </Text>
-      </View>
-
-      {pendingItems.map((item) => (
-        <PendingItemCard key={item.id} item={item} />
-      ))}
-
-      <CompletedItems />
-    </ScrollView>
+      contentInsetAdjustmentBehavior="automatic"
+      ListHeaderComponent={
+        <View style={{ gap: 14, paddingTop: 20 }}>
+          <TabScreenBackground />
+          <ListHeroCard />
+          <View className="flex-row items-center justify-between px-1">
+            <Text className="text-sm font-semibold uppercase tracking-[1px] text-muted-foreground">
+              Shopping items
+            </Text>
+            <Text className="text-sm text-muted-foreground">
+              {pendingItems.length} active
+            </Text>
+          </View>
+        </View>
+      }
+      ListFooterComponent={<CompletedItems />}
+    />
   );
 }
-
 
